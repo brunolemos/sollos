3f2305eb59936e9ca5304c78787ca483e93d2149
Pedro Goes <pedro.pecanha.goes@gmail.com>
2015-04-12 11:13:11 -0300

    Default Database

--- server/server.js
+++ server/server.js
@@ -6,17 +6,17 @@ Meteor.startup(function () {
 			loginStyle: "popup"
 		}
 	});
-	
-	if (Products.find().count() === 0) {
-	  	Products.insert({name: "Abacaxi", price: 2.99, image: "pineapples.jpg", "cooperativeId": "kabrBctibvtGGbPbE"});
-	  	Products.insert({name: "Alface", price: 0.49, image: "lettuce.jpg", "cooperativeId": "kabrBctibvtGGbPbE"});
-	  	Products.insert({name: "Banana", price: 1.99, image: "bananas.jpg", "cooperativeId": "kabrBctibvtGGbPbE"});
-	  	Products.insert({name: "Maçã", price: 0.99, image: "apples.jpg", "cooperativeId": "kabrBctibvtGGbPbE"});
-	  	Products.insert({name: "Melancia", price: 2.99, image: "watermellons.jpg", "cooperativeId": "kabrBctibvtGGbPbE"});
-	  	Products.insert({name: "Uva", price: 3.99, image: "grapes.jpg", "cooperativeId": "kabrBctibvtGGbPbE"});
-	}
 
-	if (Cooperatives.find().count() === 0) {
-		Cooperatives.insert({name: "Cooperativa COAF", image: "logo_coaf.png", "description": "Nossa missão é entregar os melhores produtos para os melhores consumidores.", "address": "Rua Francisco de Paula, 445"});
-	}
+	Cooperatives.remove({});
+	var cooperativeId = Cooperatives.insert({name: "Cooperativa COAF", image: "logo_coaf.png", cover: "240514-coaf-brasil", "description": "Nossa missão é entregar os melhores produtos para os melhores consumidores.", "address": "Rua Francisco de Paula, 445"});
+	
+	Products.remove({});
+  	Products.insert({name: "Abacaxi", price: 2.99, image: "pineapples.jpg", "cooperativeId": cooperativeId});
+  	Products.insert({name: "Alface", price: 0.49, image: "lettuce.jpg", "cooperativeId": cooperativeId});
+  	Products.insert({name: "Banana", price: 1.99, image: "bananas.jpg", "cooperativeId": cooperativeId});
+  	Products.insert({name: "Maçã", price: 0.99, image: "apples.jpg", "cooperativeId": cooperativeId});
+  	Products.insert({name: "Melancia", price: 2.99, image: "watermellons.jpg", "cooperativeId": cooperativeId});
+  	Products.insert({name: "Uva", price: 3.99, image: "grapes.jpg", "cooperativeId": cooperativeId});
+  	Products.insert({name: "Batata", price: 1.99, image: "potatos.jpg", "cooperativeId": cooperativeId});
+
 });
